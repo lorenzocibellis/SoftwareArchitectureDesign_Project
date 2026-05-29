0c895987be205d7467354e46c505fa70873c15b6
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:01:08 +0200

    Add Player class for music playlist management
    Implemented the Player class with methods for track management and state changes.

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/Player.java
@@ -0,0 +1,48 @@
+package org.unisa.musicplaylistmanager;
+
+/**
+ *
+ * @author gruppo10
+ */
+public class Player {
+    
+    // metodi d'istanza della classe Player
+    private Iterator it;
+    private Track currentTrack;
+    private PlayerState defaultState;
+    private PlayerState currentState;
+  
+    // costruttore della classe Player
+    public Player(PlayerState defaultState, null) {
+        this.defaultState = defaultState;
+    }
+    
+    // implementazione del metodo per cambiare lo stato di riproduzione
+    public void changeState() {
+        
+        
+    }
+    // 
+    public void defaultState() {
+    }
+    // implementazione del metodo che chiama l'omonimo metodo della classe Iterator per definire la modalità di riproduzione
+    public void setStrategy(ExecutionStrategy es) {
+        it.setStrategy(es);
+    }
+    
+    // implementazione del metodo che chiama l'omonimo metodo della classe Iterator per ottenere la traccia corrente
+    public Track getCurrent() {
+        
+        return it.getCurrent();
+    }
+    // implementazione del metodo che chiama l'omonimo metodo della classe Iterator per ottenere la traccia successiva
+    public Track getNext() {
+        
+        return it.getNext();
+    }
+    // implementazione del metodo che chiama l'omonimo metodo della classe Iterator per ottenere la traccia precedente
+    public Track getPrevious() {
+        
+        return it.getPrevious();
+    }
+}
