832e03db1b35efffd911a8ce176f1209a8ff3c8a
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:10:34 +0200

    Add Sequential class implementing ExecutionStrategy

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/Sequential.java
@@ -0,0 +1,14 @@
+package org.unisa.musicplaylistmanager;
+
+/**
+ *
+ * @author gruppo10
+ */
+public class Sequential implements ExecutionStrategy{
+    
+    // implementazione del metodo per eseguire la modalità di riproduzione sequenziale
+    @Override
+    public int[] execute(int a, int b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
