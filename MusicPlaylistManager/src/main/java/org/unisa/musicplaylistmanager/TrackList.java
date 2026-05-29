9f797d14f54f2c67176739c9810c91ea8248d136
MattiaSCupolo <m.cupolo3@studenti.unisa.it>
2026-05-29 12:33:40 +0200

    creazione classe TrackList

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/TrackList.java
@@ -0,0 +1,12 @@
+package org.unisa.musicplaylistmanager;
+
+public class TrackList extends Playlist{
+
+    //definizione subject per pattern Observer
+    private SubjectTrackList subjectTrackList;
+
+    //Costruttore
+    public TrackList(){
+        super(null);
+    }
+}
