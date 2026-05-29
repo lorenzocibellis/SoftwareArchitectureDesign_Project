1e0e5f83a64d0d5cdcedfc51db7cbd2a8bd526c1
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:14:39 +0200

    Add Loop class implementing ExecutionStrategy

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/Loop.java
@@ -0,0 +1,14 @@
+package org.unisa.musicplaylistmanager;
+
+/**
+ *
+ * @author gruppo10
+ */
+public class Loop implements ExecutionStrategy{
+    
+// implementazione del metodo per eseguire la modalità di riproduzione in loop
+    @Override
+    public int[] execute(int a, int b) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
