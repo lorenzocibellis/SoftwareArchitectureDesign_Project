596ae9ec468684134d191cf807244cef70aedfa2
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:22:22 +0200

    Add PlayerState interface for track playback

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/PlayerState.java
@@ -0,0 +1,11 @@
+package org.unisa.musicplaylistmanager;
+
+/**
+ *
+ * @author gruppo10
+ */
+public interface PlayerState {
+    
+    // dichiarazione del metodo per riprodurre la traccia
+    public void execute();
+}
