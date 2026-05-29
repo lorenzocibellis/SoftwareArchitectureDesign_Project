f91bb6cb5687a2a077054fbc8172f958537dfbdc
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:24:37 +0200

    Add Play class implementing PlayerState interface

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/Play.java
@@ -0,0 +1,14 @@
+package org.unisa.musicplaylistmanager;
+
+/**
+ *
+ * @author gruppo10
+ */
+public class Play implements PlayerState {
+
+    // dichiarazione del metodo per riprodurre la traccia
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
