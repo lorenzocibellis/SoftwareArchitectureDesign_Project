ef44b4ba8fc3ef5ec5210b1d4535d6c0c1bc41db
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:03:06 +0200

    Create IterableCollection.java

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/IterableCollection.java
@@ -0,0 +1,11 @@
+package org.unisa.musicplaylistmanager;
+
+/**
+ *
+ * @author gruppo10
+ */
+public interface IterableCollection {
+    
+    // dichiarazione del metodo per creare un iteratore
+    public AbstractIterator createIterator();
+}
