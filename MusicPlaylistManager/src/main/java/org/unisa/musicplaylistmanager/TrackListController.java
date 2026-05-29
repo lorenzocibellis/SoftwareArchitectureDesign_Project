4bcee1727c27ef699df25c402e3117877fe06e06
MattiaSCupolo <m.cupolo3@studenti.unisa.it>
2026-05-29 14:45:09 +0200

    prova modifica scena javafx

--- MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/TrackListController.java
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/TrackListController.java
@@ -1,8 +1,15 @@
 package org.unisa.musicplaylistmanager;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class TrackListController
 {
@@ -15,8 +22,19 @@ public class TrackListController
 
 
     @FXML
-    public void azione1(ActionEvent actionEvent) {
+    public void azione1(ActionEvent actionEvent) throws IOException {
 
-        System.out.println("azione_prova");
+        // 1. Get the Stage from the button that triggered the event
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        // 2. Load the new FXML file
+        Parent newRoot = FXMLLoader.load(getClass().getResource("TrackView.fxml"));
+
+        // 3. Create a new Scene and set it to the stage
+        Scene newScene = new Scene(newRoot);
+        stage.setScene(newScene);
+
+        // 4. Keep the stage visible
+        stage.show();
     }
 }
