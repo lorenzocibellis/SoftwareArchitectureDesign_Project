d4f53fc32e5527d196fe28e6104a3a863ddbb860
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:12:51 +0200

    Fix UnsupportedOperationException message formatting

--- MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/Sequential.java
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/Sequential.java
@@ -9,6 +9,6 @@ public class Sequential implements ExecutionStrategy{
     // implementazione del metodo per eseguire la modalità di riproduzione sequenziale
     @Override
     public int[] execute(int a, int b) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
