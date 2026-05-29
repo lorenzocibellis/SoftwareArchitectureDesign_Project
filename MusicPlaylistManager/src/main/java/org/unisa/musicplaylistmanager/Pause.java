771b2b1366f48a18dfd4e01ee19c9da124e02829
FilomenaAlfano <151449733+FilomenaAlfano@users.noreply.github.com>
2026-05-29 13:26:53 +0200

    Add Pause class implementing PlayerState interface

---
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/Pause.java
@@ -0,0 +1,14 @@
+package org.unisa.musicplaylistmanager;
+
+/**
+ *
+ * @author gruppo10
+ */
+public class Pause implements PlayerState {
+
+    // implementazione del metodo per mettere in pausa una traccia
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
