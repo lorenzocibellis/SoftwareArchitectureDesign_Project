1c2994dca282dfa4a2be6291bb07cbde1f5cf5d9
FilomenaAlfano <filomena_alfano@libero.it>
2026-05-29 15:21:01 +0200

    spostato gli ID dei TextField della TrackView dalle labels ai textfield veri e propri dove si inseriscono gli input, e impedito che l'utente inserisca stringhe nel campo relativo all'anno di pubblicazione.

--- MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/TrackController.java
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/TrackController.java
@@ -1,24 +1,42 @@
 package org.unisa.musicplaylistmanager;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.fxml.FXML;
+import javafx.scene.control.TextFormatter;
 
-public class TrackController
-{
+public class TrackController {
+
     @FXML
     private TextField anno;
+
     @FXML
     private RadioButton newRelease;
+
     @FXML
     private RadioButton contenutiEspliciti;
+
     @FXML
     private TextField titolo;
+
     @FXML
     private TextField genere;
+
     @FXML
     private TextField autore;
 
     @FXML
     public void initialize() {
-    }}
+
+        anno.setTextFormatter(
+                new TextFormatter<>(change -> {
+
+                    if (change.getControlNewText().matches("\\d{0,4}")) {
+                        return change;
+                    }
+
+                    return null;
+                })
+        );
+    }
+}
