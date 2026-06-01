ee986d614be35e3c371dc36b0ac36fefc9869bc6
MattiaSCupolo <m.cupolo3@studenti.unisa.it>
2026-06-01 11:49:54 +0200

    aggiornamento lista playlist

--- MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/PlaylistList.java
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/PlaylistList.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class PlaylistList {
 
+    //Dichiarazione Attributi
     private ArrayList<Playlist> playlistList;
 
-    
+    public void addPlaylist(Playlist playlist){
+        if (playlist)
+        playlistList.add(playlist)
+    }
 }
