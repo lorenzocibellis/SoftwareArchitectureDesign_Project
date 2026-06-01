92575fc41b7018a2ec687c2b61c856d775b04900
MattiaSCupolo <m.cupolo3@studenti.unisa.it>
2026-06-01 12:49:44 +0200

    aggiunta logica di ritorno a TrackList

--- MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/PlaylistListController.java
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/PlaylistListController.java
@@ -3,13 +3,18 @@ package org.unisa.musicplaylistmanager;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class PlaylistlistController {
+import java.io.IOException;
+
+public class PlaylistListController {
 
     @FXML
     private Button button1;
@@ -19,6 +24,9 @@ public class PlaylistlistController {
 
     @FXML
     private Button deleteButton;
+
+    @FXML
+    private Button tracksButton;
 
     @FXML
     private ListView<?> listView;
@@ -34,6 +42,21 @@ public class PlaylistlistController {
     @FXML
     void deletePlaylist(ActionEvent event) {
 
+    }
+
+    @FXML
+    public void goTrackList(ActionEvent actionEvent) throws IOException {
+        Parent playlistParent = FXMLLoader.load(getClass().getResource("TrackListView.fxml"));
+
+        // 2. Crea la nuova scena
+        Scene playlistScene = new Scene(playlistParent);
+
+        // 3. Ottieni lo stage (finestra) corrente dall'evento
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        // 4. Cambia la scena
+        window.setScene(playlistScene);
+        window.show();
     }
 
     @FXML
