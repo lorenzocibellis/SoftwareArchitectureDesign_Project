2fc44912fbe77483de46476dfd9415dcb047a72c
lorenzocibellis <l.cibellis@studenti.unisa.it>
2026-06-02 19:32:03 +0200

    aggiunta la documentazione javaDoc per il package state

--- MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/state/Pause.java
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/state/Pause.java
@@ -3,14 +3,22 @@ package org.unisa.musicplaylistmanager.state;
 import org.unisa.musicplaylistmanager.player.Player;
 
 /**
+ * Rappresenta lo stato "In Pausa" del Player (Pattern State).
+ * Quando il player è in questo stato, l'azione eseguita fermerà la riproduzione
+ * musicale e provocherà la transizione allo stato {@link Play}.
+ *
  * @author gruppo10
  */
-
 public class Pause implements PlayerState {
+    /**
+     * Esegue l'azione di pausa della riproduzione sul player.
+     * Al termine dell'azione, cambia lo stato interno del player in {@link Play}.
+     * 
+     * @param player il {@link Player} su cui mettere in pausa la riproduzione
+     */
     @Override
     public void execute(Player player) {
         player.stopPlayback();
-        // Cambia lo stato interno del player al prossimo stato logico
         player.setState(new Play()); 
     }
 }
--- MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/state/PlayerState.java
+++ MusicPlaylistManager/src/main/java/org/unisa/musicplaylistmanager/state/PlayerState.java
@@ -3,10 +3,18 @@ package org.unisa.musicplaylistmanager.state;
 import org.unisa.musicplaylistmanager.player.Player;
 
 /**
+ * Interfaccia comune per gli stati del Player (Pattern State).
+ * Definisce il comportamento che deve essere implementato da ogni stato 
+ * per poter eseguire azioni specifiche sul player.
+ *
  * @author gruppo10
  */
-
 public interface PlayerState {
-    // Riceve il Player per poterne manipolare lo stato interno
+    /**
+     * Esegue l'azione specifica associata a questo stato e gestisce
+     * l'eventuale transizione allo stato successivo.
+     * 
+     * @param player il {@link Player} su cui agire
+     */
     void execute(Player player);
 }
